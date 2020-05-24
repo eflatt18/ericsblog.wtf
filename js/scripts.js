0b50b03b9037c51bed74f3e079d9e621031a6d8a
eflatt18 <eflatt18@gmail.com>
2020-05-24 16:09:36 -0400

    features and functions

--- js/scripts.js
+++ js/scripts.js
@@ -12,3 +12,23 @@ toggle.addEventListener('change', e => {
 function goAway() {
     document.getElementById("mode").style.display = "none";
   }
+
+function me() {
+    document.getElementById("me").style.display = "block";
+  }
+  
+function meInvisible() {
+    document.getElementById("me").style.display = "none";
+  }
+
+  function meTwo() {
+    document.getElementById("meTwo").style.display = "block";
+  }
+  
+function meInvisibleTwo() {
+    document.getElementById("meTwo").style.display = "none";
+  }  
+
+  $(document).mousemove(function(e){
+    $("#image").css({left:e.pageX, top:e.pageY});
+});
